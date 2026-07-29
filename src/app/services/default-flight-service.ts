af488c41254621c536c184fbc9686e297580240e
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2026-07-29 14:00:53 +0200

    25 – Create an Injection Token for the Base URL

--- src/app/services/default-flight-service.ts
+++ src/app/services/default-flight-service.ts
@@ -1,12 +1,17 @@
-import { Injectable, Signal } from '@angular/core';
+import { inject, Injectable, InjectionToken, Signal } from '@angular/core';
 import { FlightSearchCriteria } from '../entities/criteria';
 import { httpResource } from '@angular/common/http';
 import { Flight } from '../entities/flight';
 import { FlightService } from './flight-service';
 
+export const BASE_URL = new InjectionToken<string>('BASE_URL', {
+    providedIn: 'root',
+    factory: () => 'https://demo.angulararchitects.io/api/flight'
+});
+
 @Injectable()
 export class DefaultFlightService implements FlightService {
-    readonly #url = 'https://demo.angulararchitects.io/api/flight';
+    readonly #url = inject(BASE_URL);
 
     createFlightsResource(searchCriteria: Signal<FlightSearchCriteria>) {
         return httpResource<Flight[]>(() => ({
