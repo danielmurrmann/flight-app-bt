b8f6fbe5157ec6f757de4d5b36fdfe99d841a6ae
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2026-07-29 11:40:06 +0200

    23 – Implement a Factory for the FlightService

---
+++ src/app/services/dummy-flight-service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Resource, resourceFromSnapshots, ResourceSnapshot, signal, Signal } from '@angular/core';
+import { FlightSearchCriteria } from '../entities/criteria';
+import { httpResource } from '@angular/common/http';
+import { Flight } from '../entities/flight';
+import { FlightService } from './flight-service';
+
+@Injectable()
+export class DummyFlightService implements FlightService {
+    createFlightsResource(searchCriteria: Signal<FlightSearchCriteria>) {
+        let resourceData = signal<ResourceSnapshot<Flight[]>>({
+            value: [
+                { id: 1, from: 'Hamburg', to: 'Graz', date: '2024-06-01', delayed: false },
+                { id: 2, from: 'Hamburg', to: 'Graz', date: '2024-06-02', delayed: true },
+            ],
+            status: 'local'
+        });
+        return resourceFromSnapshots(resourceData);
+    }
+}
--- src/app/services/flight-service.ts
+++ src/app/services/flight-service.ts
@@ -1,13 +1,18 @@
-import { Injectable, Signal } from '@angular/core';
+import { Injectable, Resource, Signal } from '@angular/core';
 import { FlightSearchCriteria } from '../entities/criteria';
 import { httpResource, HttpResourceRef } from '@angular/common/http';
 import { Flight } from '../entities/flight';
 import { DefaultFlightService } from './default-flight-service';
+import { DummyFlightService } from './dummy-flight-service';
 
 @Injectable({
     providedIn: 'root',
-    useClass: DefaultFlightService
+    useFactory: () => {
+        let useDummyValues = false; // inject(ConfigService).config.useDummyValues;
+        if(useDummyValues) return new DummyFlightService();
+        else return new DefaultFlightService();
+    }
 })
 export abstract class FlightService {
-    abstract createFlightsResource(searchCriteria: Signal<FlightSearchCriteria>): HttpResourceRef<Flight[]>
+    abstract createFlightsResource(searchCriteria: Signal<FlightSearchCriteria>): Resource<Flight[]>
 }
