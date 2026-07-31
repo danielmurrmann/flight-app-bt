57a7da8c5d0758eae2e0b5dfae4ea9a5c058feee
Daniel Murrmann <daniel.murrmann@fancy-development.net>
2026-07-31 13:36:58 +0200

    73 – Implement Programatic Routing at the HomeView

--- src/app/home-view/home-view.ts
+++ src/app/home-view/home-view.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-view',
   imports: [],
   templateUrl: './home-view.html',
 })
-export class HomeView {}
+export class HomeView {
+  private readonly router = inject(Router);
+
+  navigateToFlightSearch() {
+    this.router.navigate(['/flight-search']);
+  }
+}
